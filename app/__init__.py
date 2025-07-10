870ba9c49b90cecf14e490e5fe3ffcd834c0beed
alexandrumariusionut <alexandrumariusmail@gmail.com>
2025-07-10 18:09:31 +0300

    Create __init__.py

---
+++ app/__init__.py
@@ -0,0 +1,18 @@
+# app/__init__.py
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'supersecret123'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///angajati.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.init_app(app)
+
+    from .routes import main
+    app.register_blueprint(main)
+
+    return app
